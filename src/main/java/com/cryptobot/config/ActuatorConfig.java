efc56a5ac2caea3e1235a901c51c162fed6c652c
jherreraportilla <jherrerap97@gmail.com>
2025-12-05 15:53:54 +0100

    Fix cambio de ActuatorConfig

--- src/main/java/com/cryptobot/config/ActuatorConfig.java
+++ src/main/java/com/cryptobot/config/ActuatorConfig.java
@@ -3,12 +3,14 @@ package com.cryptobot.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class ActuatorExposureConfig {
+public class ActuatorConfig {
 
     @Bean
-    public WebEndpointProperties exposeAllEndpoints() {
+    @Primary
+    public WebEndpointProperties actuatorWebEndpointProperties() {
         WebEndpointProperties props = new WebEndpointProperties();
         props.getExposure().getInclude().add("*");
         return props;
