d996b4e6cf20f8469c8f8dd85a5bf8c254af015b
jherreraportilla <jherrerap97@gmail.com>
2025-12-05 11:16:56 +0100

    Commit fix BtcBotApplication usando la anotacion @EnableScheduling

--- src/main/java/com/cryptobot/BtcBotApplication.java
+++ src/main/java/com/cryptobot/BtcBotApplication.java
@@ -2,24 +2,12 @@ package com.cryptobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.cryptobot.notification.WhatsAppNotifier; // IMPORTANTE
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class BtcBotApplication implements CommandLineRunner {
-
-    @Autowired
-    private WhatsAppNotifier whatsAppNotifier;
-
+@EnableScheduling
+public class BtcBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(BtcBotApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        // Se ejecuta cuando Spring ya está completamente iniciado
-        whatsAppNotifier.sendMessage("LA YAMPI CULISHEA MUCHO");
     }
 }
